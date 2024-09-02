282c96ac18e55c1a6e766d8c7049abc1a2974556
Dev.Hwang <zxc1063@naver.com>
2024-09-02 11:43:53 +0900

    Remove duplicate scripts

--- Template Project/Assets/_Assets/Scripts/NetworkStateChecker/NetworkStateChecker.cs
+++ Template Project/Assets/_Assets/Scripts/NetworkStateChecker/NetworkStateChecker.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using MyLibrary.DesignPattern;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Networking;
